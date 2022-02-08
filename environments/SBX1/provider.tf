ceb1ee5820a7ff278be715a8a381c0d0b5dab556
rpaskalev <rpaskalev@lomixtech.com>
2022-02-07 20:59:41 -0500

    added ci/cd config

---
+++ environments/SBX1/provider.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      Name        = "ziyotek-devops-class"
+      Environment = var.environment
+    }
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "rady-bucket-1-0-0"
+    key    = "dags/myfile"
+    region = "us-east-1"
+  }
+}
